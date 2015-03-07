e07ca7473822b6c6aa5ded919e4e81e00ae7a696
Kelvin N <kelvin@kelvinism.com>
2015-03-07 18:44:46 +1100

    Sample test to show in snap

---
+++ test/sample.spec.js
@@ -0,0 +1,14 @@
+describe("The 'baby-tech' team", function() {
+  it("has adam", function() {
+    var name = "adam";
+    expect(name).toEqual("adam");
+  });
+  it("has kelvin", function() {
+    var name = "kelvin";
+    expect(name).toEqual("kelvin");
+  });
+  it("does not have anthony", function() {
+    var name = "anthony";
+    expect(name).toNotEqual("spock");
+  });
+});
