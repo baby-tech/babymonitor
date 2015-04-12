c4a2483260649385e7ebf4f4c46a9f5439ddfc50
Adam Scott <me@adams.co.tt>
2015-04-12 09:36:56 +0000

    Scaling back image size.

--- app/constants.js
+++ app/constants.js
@@ -1,8 +1,8 @@
 var path = require('path');
 
 module.exports = {
-  WIDTH: 2592,
-  HEIGHT: 1944,
+  WIDTH: 1600,
+  HEIGHT: 1200,
   INTERVAL: 1950,
   IMAGE_PATH: path.resolve(__dirname + '/../images/camera.jpg')
 };
