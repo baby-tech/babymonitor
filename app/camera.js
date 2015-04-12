689e26f12d0314f2877ea9e412dafc0af41644f1
Adam Scott <me@adams.co.tt>
2015-04-12 11:25:10 +0000

    Clean up.

--- app/camera.js
+++ app/camera.js
@@ -5,16 +5,26 @@ module.exports = {
   run: function () {
     'use strict';
 
-    function takePhoto() {
-      var command = ['raspistill', '--width', constants.WIDTH, '--height', constants.HEIGHT, '--output', constants.IMAGE_PATH, '--quality', '50', '--rotation', '180'].join(' ');
-      console.log(command);
-      exec(command, function (err) {
-        if (err) {
-          throw err;
-        }
-        setTimeout(takePhoto, constants.INTERVAL);
-      });
+    var command = [
+      'raspistill',
+      '--width', constants.WIDTH,
+      '--height', constants.HEIGHT,
+      '--output', constants.IMAGE_PATH,
+      '--quality', '50',
+      '--rotation', '180'
+    ].join(' ');
+
+    function handleExit(err) {
+      if (err) {
+        throw err;
+      }
+      setTimeout(takePhoto, constants.INTERVAL);
     }
+
+    function takePhoto() {
+      exec(command, handleExit);
+    }
+
     takePhoto();
   }
 };
